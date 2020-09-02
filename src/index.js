9a05cf713421080b2b152a8dd102fde035536fc9
unknown <adityaparsad01@gmail.com>
2020-09-02 16:54:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import App from "./Emp/App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById("root")
 );
